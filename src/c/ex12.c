da2482111d9fcef254c61935d7ab20384e5ff9bd
James Patrizi <jp15mail66@gmail.com>
2015-06-19 11:22:25 -0400

    ex12 and ec is done. if and else statements ex.

---
+++ src/c/ex12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	//ec
+	if(argc ==1){
+		printf("You do not have an arguement.\n");
+	}
+	else if(argc ==2){
+		printf("You only have one arguement. You suck.\n");
+	}
+
+	else if(argc > 1 && argc <4){
+		printf("Here's your arguments:\n");
+		for(int i = 1; i < argc ; i++){
+			printf("%s ",argv[i]);
+		}
+		printf("\n");
+	}
+	//ec
+	else if(argc > 1 && argc >5){
+		printf("Warning you are about to have to many arguments. At argument #5\n");
+	}
+
+	else {
+		printf("You have too many arguments. You suck. \n");
+	}
+	
+	return 0;
+}
